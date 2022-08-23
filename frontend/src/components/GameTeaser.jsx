dfaa0ada264f31e5dce6a46f55f594d237fdea72
Dalton Eggleston <egglestondalton@yahoo.com>
2022-08-23 11:03:28 -0700

    added black star

--- frontend/src/components/GameTeaser.jsx
+++ frontend/src/components/GameTeaser.jsx
@@ -1,6 +1,7 @@
 import { Col } from "react-bootstrap"
 
 import star from '../assets/star.png'
+import emptyStar from '../assets/blackstar.png'
 
 
 function GameTeaser(props){
@@ -14,6 +15,7 @@ function GameTeaser(props){
         <img src={game.image} alt="" />
         <br />
         {[...Array(game.rating_top)].map(() => <img width='60px' height='60px' className="game-rating" src={star}></img>)}
+        {game.rating_top != 5 && [...Array(5 - game.rating_top)].map(() => <img width='60px' height='60px' className="game-rating" src={emptyStar}></img>)}
 
         
         
