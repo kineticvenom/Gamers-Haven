d04cb659ce82b4bf517708c0451bbb1cca81c107
Dalton Eggleston <egglestondalton@yahoo.com>
2022-08-29 08:57:18 -0700

    home screen change

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -5,10 +5,15 @@ import {Row,Col} from 'react-bootstrap/'
 
 function HomePage(props) {
   const { user, saveData, setSaveData } = props
+  
+  
   const getFeedData = () =>{
     axios.get('feed/get').then((response) =>{
-     
-      setSaveData(response.data)
+      for (let category in response.data){ // Only sets the Save Data if the user has some sort of activity
+        if (response.data[category].length > 0){
+          setSaveData(response.data)
+        }
+      }
     })
   }
 
@@ -17,6 +22,7 @@ function HomePage(props) {
   },[])
 
 
+  console.log(saveData)
 
   return ( 
     <div>
@@ -25,7 +31,8 @@ function HomePage(props) {
       <h1>Welcome to Gamers Haven!</h1>
     }
       <hr/>
-      {user && saveData ? 
+      {user ? 
+      saveData ? 
         <div>
           {saveData.posts.map((post) => (
               <GamePost {...post}   /> 
@@ -50,9 +57,10 @@ function HomePage(props) {
               <br/>
             </div>
           ))}
-        </div>: <h4>loading feed...</h4>
+        </div>: <h4>Loading feed...</h4> : <h1>Please login or create an account to see your activity</h1>}
+      
 
-      }
+      
 
     
     </div>
